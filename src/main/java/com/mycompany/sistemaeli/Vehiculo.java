7f5e0c7be784f0c63c9f564bff0817cf6fe47fa9
santiagoneriz-uni <santiagoneriz@fiuni.edu.py>
2026-05-19 01:01:40 -0300

    Update Vehiculo.java

--- src/main/java/com/mycompany/sistemaeli/Vehiculo.java
+++ src/main/java/com/mycompany/sistemaeli/Vehiculo.java
@@ -2,16 +2,21 @@ package com.mycompany.sistemaeli;
 
 public abstract class Vehiculo {
 
-    private final String id;
+    private String id;  //Cambio: ya no es final
 
     //Constructor
     public Vehiculo(String id) {
         this.id = id;
     }
 
-    //Metodo concreto (reutilizable)
+    //Getter
     public String getId() {
         return id;
+    }
+    
+    //Setter - nuevo metodo para poder modificar el ID
+    public void setId(String id) {
+        this.id = id;
     }
 
     //Metodo abstracto (obliga a las subclases)
